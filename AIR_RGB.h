5a848b377a6d553c3494a5482628d5859e9364ac
张逸飞 <1103536250@qq.com>
2024-05-14 08:57:53 +0800

    主板代码初步完成

---
+++ AIR_RGB.h
@@ -0,0 +1,109 @@
+#ifndef AIR_RGB_H
+#define AIR_RGB_H
+#include <FastLED.h>
+#include "HID-Project.h"
+#define NUM_LEDS 33
+#define DATA_PIN A5
+CRGB leds[NUM_LEDS];
+
+#define air_right_pin 5
+#define air_right_num 6
+CRGB AIR_RIGHT[air_right_num];
+
+#define air_left_pin 9
+#define air_left_num 6
+CRGB AIR_LEFT[air_left_num];
+
+uint8_t air_state[6];
+
+uint16_t airkey[6] = {'/', '.', '\'', ';', ']', '['};
+// uint16_t airpin[6] = {7, 6, 12, 10, 13, 11};
+int data[6];
+uint8_t airRelease[6];
+uint8_t airPress[6];
+long air_time = 0;
+
+void RGBset() {
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, air_right_pin, GRB>(AIR_RIGHT, air_right_num);
+  FastLED.addLeds<WS2812B, air_left_pin, GRB>(AIR_LEFT, air_left_num);
+}
+
+uint8_t air_rgb_loop = 0;
+uint16_t air_colour = 0;
+uint16_t air_colour_state = 0;
+void air_rgb_Serial() {
+  AIR_RIGHT[air_rgb_loop] = CHSV(air_colour_state, 255, 255);
+  AIR_LEFT[air_rgb_loop] = CHSV(air_colour_state, 255, 255);
+  air_rgb_loop++;
+  if (air_rgb_loop >= 6) {
+    air_colour_state++;
+    air_rgb_loop = 0;
+  }
+  if (air_colour_state > 255) {
+    air_colour_state = 0;
+  }
+  for (uint8_t i = 0; i < 6; i++) {
+    if (air_state[i]) {
+      AIR_RIGHT[i] = 0xffffff;
+      AIR_LEFT[i] = 0xffffff;
+    }
+  }
+  FastLED[1].showLeds(255);
+  FastLED[2].showLeds(255);
+}
+void airset() {
+  for (uint8_t i = 0; i < 6; i++) {
+    // pinMode(airpin[i], INPUT_PULLUP);
+    airPress[i] = 0;
+    airRelease[i] = 0;
+    air_state[i] = 0;
+  }
+}
+
+void serial_cheak() {
+  if (Serial.available()) {
+     uint16_t = serial.readStringUntil('\n');
+    dataString.trim(); // 去除首尾空格
+    // 将字符串解析为整数数组
+    int index = 0;
+    char *ptr = strtok(const_cast<char*>(dataString.c_str()), " ");
+    while (ptr != NULL) {
+      data[index++] = atoi(ptr);
+      ptr = strtok(NULL, " ");
+    }
+    // 在这里使用接收到的数据进行相应的处理
+    // 例如：检查哪个0变为了1，并执行相应的操作
+    for (int i = 0; i < 6; i++) {
+      Serial.print(data[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+}
+
+
+void aircheck() {
+  for (uint8_t i = 0; i < 6; i++) {
+    uint8_t airstate;
+    airstate = data[i];
+    if (airstate == 0) {
+      if (!airPress[i]) {
+        NKROKeyboard.press(airkey[i]);
+        airRelease[i] = 0;
+        airPress[i] = 1;
+        air_state[i] = 1;
+      }
+    }
+    else {
+      if (!airRelease[i]) {
+        NKROKeyboard.release(airkey[i]);
+        airRelease[i] = 1;
+        airPress[i] = 0;
+        air_state[i] = 0;
+      }
+      continue;
+    }
+  }
+}
+#endif
