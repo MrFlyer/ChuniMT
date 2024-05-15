00fbbd2fbe1dd53b77f59ae7acd1d32d4f8fd40b
张逸飞 <1103536250@qq.com>
2024-05-15 17:45:28 +0800

    针对Air001的接收端做出对应适配

--- AIR/AIR.ino
+++ AIR/AIR.ino
@@ -1,6 +1,6 @@
-int ADCNUM[6] = {A0, A1, A2, A3, A4, A5};
+int ADCNUM[6] = {PA0, PA1, PA4, PA5, PA6, PA7};
 void setup() {
-  serial.begin(9600);
+  Serial.begin(9600);
   for (int i = 0; i < 6; i++) {
     pinMode(ADCNUM[i], INPUT_PULLUP);
   }
@@ -12,11 +12,11 @@ void loop() {
     int ADCread = analogRead(ADCNUM[i]);
     if (ADCread >= 850) {
       adcData[i] = 1;
-      serial.print(adcData[i]);
+      Serial.print(adcData[i]);
     }
     else {
       adcData[i] = 0;
-      serial.print(adcData[i]);
+      Serial.print(adcData[i]);
     }
     if (i < 5) {
       serial.print(" ");
