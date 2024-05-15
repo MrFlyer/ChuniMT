a7166da0ce956c51a987c0e17ca7326cff6ac92a
张逸飞 <1103536250@qq.com>
2024-05-15 17:29:43 +0800

    修改接收端代码

--- AIR/AIR.ino
+++ AIR/AIR.ino
@@ -1,17 +1,26 @@
 int ADCNUM[6] = {A0, A1, A2, A3, A4, A5};
 void setup() {
+  serial.begin(9600);
   for (int i = 0; i < 6; i++) {
     pinMode(ADCNUM[i], INPUT_PULLUP);
   }
 }
 
 void loop() {
+  int adcData[6] = {0, 0, 0, 0, 0, 0};
   for (int i = 0; i < 6; i++) {
     int ADCread = analogRead(ADCNUM[i]);
     if (ADCread >= 850) {
-      digitalWrite(pinNUM[i + 6], LOW);
+      adcData[i] = 1;
+      serial.print(adcData[i]);
     }
     else {
-      digitalWrite(pinNUM[i + 6], HIGH);
+      adcData[i] = 0;
+      serial.print(adcData[i]);
+    }
+    if (i < 5) {
+      serial.print(" ");
     }
   }
+  serial.println();
+}
